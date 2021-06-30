5988534a1c8b21af188fb148acdab6e10cb41171
christiaan-janssen <christiaanjanssen@outlook.com>
2021-06-30 07:51:03 +0200

    Move the player

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ struct Position {
 }
 
 #[derive(Component)]
-#[derive(Debug)]
 struct Renderable {
     glyph: rltk::FontCharType,
     fg: RGB,
@@ -19,6 +18,9 @@ struct Renderable {
 
 #[derive(Component)]
 struct LeftMover{}
+
+#[derive(Component, Debug)]
+struct Player {}
 
 struct State {
     ecs: World
@@ -48,7 +50,7 @@ impl<'a> System<'a> for LeftWalker {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-
+        player_input(self, ctx);
         self.run_systems();
 
         let position = self.ecs.read_storage::<Position>();
@@ -57,6 +59,30 @@ impl GameState for State {
         for (pos, render) in (&position, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x +delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happend
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
     }
 }
 
@@ -71,6 +97,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
@@ -79,7 +106,9 @@ fn main() -> rltk::BError {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
-        }).build();
+        })
+    .with(Player{})
+    .build();
     
     for i in 0..10{
         gs.ecs
