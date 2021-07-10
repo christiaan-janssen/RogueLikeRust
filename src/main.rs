7e78c57d143937c6fbcda7ea0b57bd67437bf8c9
christiaan-janssen <christiaanjanssen@outlook.com>
2021-07-10 09:05:24 +0200

    Add some simple monster

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,13 @@ impl GameState for State {
 
         let position = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Map>();
 
         for (pos, render) in (&position, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
     }
 }
@@ -55,7 +59,6 @@ fn main() -> rltk::BError {
 
     let map: Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
-    gs.ecs.insert(map);
 
     gs.ecs
         .create_entity()
@@ -75,6 +78,34 @@ fn main() -> rltk::BError {
             dirty: true,
         })
         .build();
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        let glyph: rltk::FontCharType;
+        let roll = rng.roll_dice(1, 2);
+
+        match roll {
+            1 => glyph = rltk::to_cp437('g'),
+            _ => glyph = rltk::to_cp437('o'),
+        }
+	
+        gs.ecs
+            .create_entity()
+            .with(Position { x, y })
+            .with(Renderable {
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed {
+                visible_tiles: Vec::new(),
+                range: 8,
+                dirty: true,
+            })
+            .build();
+    }
+    gs.ecs.insert(map);
 
     rltk::main_loop(context, gs)
 }
