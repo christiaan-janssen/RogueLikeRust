0271034c3376733e7806e8ed4d00fac6d7da28f3
christiaan-janssen <christiaanjanssen@outlook.com>
2021-07-19 08:04:22 +0200

    Give monsters a name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 use rltk::{GameState, Point, RGB, Rltk};
 use specs::prelude::*;
 
@@ -67,11 +69,13 @@ fn main() -> rltk::BError {
 	ecs: World::new(),
 	runstate: RunState::Running,
     };
+    
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
     gs.ecs.register::<Monster>();
+    gs.ecs.register::<Name>();
 
     let map: Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
@@ -93,17 +97,19 @@ fn main() -> rltk::BError {
             range: 8,
             dirty: true,
         })
+        .with(Name{name: "Player".to_string()})
         .build();
 
     let mut rng = rltk::RandomNumberGenerator::new();
-    for room in map.rooms.iter().skip(1) {
+    for (i, room) in map.rooms.iter().skip(1).enumerate() {
         let (x, y) = room.center();
+	
         let glyph: rltk::FontCharType;
+	let name: String;
         let roll = rng.roll_dice(1, 2);
-
         match roll {
-            1 => glyph = rltk::to_cp437('g'),
-            _ => glyph = rltk::to_cp437('o'),
+            1 => { glyph = rltk::to_cp437('g'); name = "Goblin".to_string(); }
+            _ => { glyph = rltk::to_cp437('o'); name = "Orc".to_string(); }
         }
 	
         gs.ecs
@@ -120,6 +126,7 @@ fn main() -> rltk::BError {
                 dirty: true,
             })
 	    .with(Monster{})
+            .with(Name{ name: format!("{} #{}", &name, i)})
             .build();
     }
     gs.ecs.insert(map);
--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,5 +1,5 @@
 use specs::prelude::*;
-use super::{Viewshed, Position,Monster};
+use super::{Viewshed, Monster, Name};
 use rltk::{Point, console};
 
 pub struct MonsterAI {}
@@ -8,14 +8,15 @@ impl<'a> System<'a> for MonsterAI {
     type SystemData = (
 	ReadExpect<'a, Point>,
 	ReadStorage<'a, Viewshed>,
-	ReadStorage<'a, Monster>);
+	ReadStorage<'a, Monster>,
+	ReadStorage<'a, Name>);
 
     fn run(&mut self, data: Self::SystemData) {
-	let (player_pos, viewshed, monster) = data;
+	let (player_pos, viewshed, monster, name) = data;
 
-	for (viewshed, _monster) in (&viewshed, &monster).join() {
+	for (viewshed, _monster, name) in (&viewshed, &monster, &name).join() {
 	    if viewshed.visible_tiles.contains(&*player_pos) {
-		console::log("Monster considers their own existence");
+		console::log(&format!("{} shouts insutls!", name.name));
 	    }
 	}
     }
